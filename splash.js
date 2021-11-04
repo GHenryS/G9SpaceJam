d37bea16d884ddee90571b45773e6ea28485f516
GHenryS <he.ge.smith@gmail.com>
2021-11-04 19:54:45 +1000

    again

--- splash.js
+++ splash.js
@@ -15,7 +15,7 @@ function splashScreen(){
 
 // ------------------ FUNCTION TO PLAY INTRO SCREEN ---------------------- //
 function introScreen(){
-<<<<<<< HEAD
+
   // scale to fit the screen
   createCanvas(window.innerWidth, window.innerWidth * canRatio)
 
@@ -30,39 +30,37 @@ function introScreen(){
   strokeWeight(0.5);
   stroke(0,255,0);
   fill(0,255,0);
-=======
+
   textFont(SplashScreenFont);
   textAlign(CENTER);
   textSize(40);
   strokeWeight(0.5);
   stroke(0 , 255 , 0);
   fill(0 , 255 , 0);
->>>>>>> 3916ee88c0553c3ba956204b986dcb378a32ec87
+
   
   //  This if/else statement scrolls the screen down
   //  To adjust how farthe screen will scorll adjust the if statement 
   //  To adjust the scroll speed adjust the frame count multiplier
-<<<<<<< HEAD
-  if(frameCount < 680){
-    image(introImage, 0,0 - frameCount * 1, 2 * width, 2 * height);
-  } else {
-    //Once the image has achieved the desired point it will be redrawn
-    //at that point
-    image(introImage, 0,-680, 2 * width, 2 * height);
-=======
 
   if(frameCount < 680){
-      image(IntroImage , 0,0 - frameCount * 1 , 2 * width , 2 * height);
-      }else{
+    image(introImage, 0,0 - frameCount * 1, 2 * width, 2 * height);
+  } else {    //Once the image has achieved the desired point it will be redrawn
+    //at that point
+    image(introImage, 0,-680, 2 * width, 2 * height);
+  }
+
+  if(frameCount < 680){
+    image(IntroImage , 0,0 - frameCount * 1 , 2 * width , 2 * height);
+    } else {
   //Once the image has achieved the desired point it will be redrawn
   //at that point
-      image(IntroImage , 0 ,0 - 680 , 2 * width , 2 * height);
->>>>>>> 3916ee88c0553c3ba956204b986dcb378a32ec87
+    image(IntroImage , 0 ,0 - 680 , 2 * width , 2 * height);
   }
   
   // Introduction of Collaborators, note the 10 second
   // intervals in between each text being shown
-<<<<<<< HEAD
+
   if(frameCount<60){                          // 60 FrameCount
       text("Presenting", width / 2,height * 0.2);
   }else if(frameCount > 70 && frameCount < 215){   // 145 FrameCount
@@ -97,7 +95,7 @@ function introScreen(){
       for(i=0; i < 1 ; i++){
           let xpos    = r * cos(theta[i] + 1.4 * PI) + width * 0.4;   // X position of rocketship
           let ypos    = r * sin(theta[i] + 1.4 * PI) + height * 2.5; // Y position of rocketship
-=======
+
   if(frameCount < 60){                          // 60 FrameCount
       text("Presenting" , width / 2 , height * 0.2);
   }else if(frameCount > 70 && frameCount < 215){   // 145 FrameCount
@@ -128,33 +126,23 @@ function introScreen(){
   //At thia point the rocketship will come into view from
   //left screen
   if(frameCount > 700){
-      r = width * 1.5;   // Size of the radius of the orbit 
-      for(i=0 ; i < 1 ; i++){
-          let xpos = r * cos(theta[i] + 1.4 * PI) + width * 0.4;   // X position of rocketship
-          let ypos = r * sin(theta[i] + 1.4 *PI) + height * 3.5; // Y position of rocketship
->>>>>>> 3916ee88c0553c3ba956204b986dcb378a32ec87
-          // X and Y positioning uses a polar coordinate system which can be
-          // represented as Xposition = r * cos (theta[i]+a*PI)+width*b
-          // and            Yposition = r * sin (theta[i]+a*PI)+height*b
-          // The 'a' value will adjust how far around the circle the rocketship will appear
-          // The 'b' value will adjust the X and Y position of the center of the rocketships orbit
-<<<<<<< HEAD
-          rocketship  = createSprite(xpos,ypos);
-          rocketship.addImage(rocketImage);
-          rocketship.rotation = 80 + ((frameCount - 700) / 55); // This is a ham-fisted attempt to keep the ship level with the planet
-          theta[i] += 0.0005;  // This will adjust the speed of the ship 
-        }
-  drawSprite(rocketship);
-  }
-  // gameState           = "mainmenu";
-=======
-          rocketship = createSprite(xpos,ypos);
-          rocketship.addImage(RocketImage2);
-          rocketship.rotation = 80 + ((frameCount - 700) / 55); // This is a ham-fisted attempt to keep the ship level with the planet
-          theta[i] += 0.0005; // This will adjust the speed of the ship 
-        }
-  drawSprite(rocketship);
-  }
->>>>>>> 3916ee88c0553c3ba956204b986dcb378a32ec87
-}
+    r = width * 1.5;   // Size of the radius of the orbit 
+    for(i=0 ; i < 1 ; i++){
+        let xpos = r * cos(theta[i] + 1.4 * PI) + width * 0.4;   // X position of rocketship
+        let ypos = r * sin(theta[i] + 1.4 *PI) + height * 3.5; // Y position of rocketship
 
+        // X and Y positioning uses a polar coordinate system which can be
+        // represented as Xposition = r * cos (theta[i]+a*PI)+width*b
+        // and            Yposition = r * sin (theta[i]+a*PI)+height*b
+        // The 'a' value will adjust how far around the circle the rocketship will appear
+        // The 'b' value will adjust the X and Y position of the center of the rocketships orbit
+
+        rocketship  = createSprite(xpos,ypos);
+        rocketship.addImage(rocketImage);
+        rocketship.rotation = 80 + ((frameCount - 700) / 55); // This is a ham-fisted attempt to keep the ship level with the planet
+        theta[i] += 0.0005;  // This will adjust the speed of the ship 
+      }
+    }
+  drawSprite(rocketship);
+  }
+}
