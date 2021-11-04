902b7a87dac2793b57a03ce27922e09be2caec03
Todd <todd.lonergan@griffithuni.edu.au>
2021-11-04 20:10:15 +1000

    More repairs 8:10pm 4/11

--- splash.js
+++ splash.js
@@ -1,5 +1,17 @@
 /* Contributor = Todd */
+// ------------------ FUNCTION TO SETUP INTRO SCREEN ---------------------- //
 
+function introScreenSetup(){
+    // setup page
+    createCanvas(window.innerWidth - 4 , window.innerHeight - 4);
+    background(0,0,0);
+    // create/clear a temporary array
+    theta                 = []; 
+    // fill theta array for all spinning things
+    for(let i=0 ; i < 360 ; i++){   
+      theta[i]            = i;  
+    }
+}
 
 // ------------------ FUNCTION TO SETUP INTRO SCREEN ---------------------- //
 
