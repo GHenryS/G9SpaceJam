4abbbd0df51154dbb9c96fb634169dff1e8001aa
GHenryS <he.ge.smith@gmail.com>
2021-11-11 12:19:21 +1000

    added the Human Class

--- humanClassTest.js
+++ humanClassTest.js
@@ -1,4 +1,21 @@
 
+
+// varables
+let mainCharacter;              // declare variable to represent the main
+
+function setup(){
+    createCanvas(300,300);
+    mainCharacter               =   new HumanObject(150, 150, 2); // create the mainCharacter using the HumanObject class
+}
+
+function draw(){
+    background(0,0,0);
+
+    //mainCharacter.move();  
+    mainCharacter.draw();
+
+}
+
 class HumanObject{
     // the constructor is like the setup function in P5
     constructor(xPos, yPos, size){
