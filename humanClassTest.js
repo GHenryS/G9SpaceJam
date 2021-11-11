c152268d60d67879cc58e8f974770d52a939f4da
GHenryS <he.ge.smith@gmail.com>
2021-11-11 22:34:39 +1000

    prrr

--- humanClassTest.js
+++ humanClassTest.js
@@ -5,7 +5,7 @@ let mainCharacter;              // declare variable to represent the main
 
 function setup(){
     createCanvas(300,300);
-    mainCharacter               =   new HumanObject(150, 150, 2); // create the mainCharacter using the HumanObject class
+    mainCharacter               =   new HumanObject(150, 150, 3); // create the mainCharacter using the HumanObject class
 }
 
 function draw(){
